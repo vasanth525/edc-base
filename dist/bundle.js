92a83b9de07a18f9522fae682875719b1bbe68ef
vasanthakumar <kvasanth52575@gmail.com>
2022-12-04 19:47:48 +0530

    update umd bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,8 @@
-define(['exports'], (function (exports) { 'use strict';
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.edc_base_ts = {}));
+})(this, (function (exports) { 'use strict';
 
     //base core component
     class ComponentBase {
